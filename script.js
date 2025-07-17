80173fe7c406c52883ba24297d503fc0e409d5d7
Diego Gomez <diego.gomez5916@my.riohondo.edu>
2025-07-17 10:28:28 -0700

    fixed text flicker bug

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function typeWriter(text, element, i = 0, callback = null) {
 const flicker = () => {
     const text = document.getElementById("flicker-text");
     const random = Math.random();
-    text.style.opacity = random < 0.75 ? 1 : 0.85;
+    text.style.opacity = random < 0.75 ? 1 : 0.8;
 };
 
 const bigFlicker = () => {
@@ -66,21 +66,20 @@ const bigFlicker = () => {
     text.style.opacity = random < 0.95 ? 1 : 0.75;
 };
 
-setInterval(flicker, 100);
-setInterval(bigFlicker, 150);
-
 typeWriter(bootSequence, document.getElementById("boot-sequence"), 0, () => {
     window.scrollTo(0, 0)
     document.getElementById("html").style.scrollBehavior = "smooth";
     bodyElement.innerHTML = bodyContent;
-        const mainHeading = document.getElementById("main-heading");
-        const mainSubheading = document.getElementById("main-subheading");
-        const typingContent = [mainHeading.textContent, mainSubheading.textContent];
-        mainHeading.textContent = "";
-        mainSubheading.textContent = "";
-        typingSpeed = 50;
-        newLineSpeed = 250;
-        typeWriter(typingContent[0], mainHeading, 0, () => {
-            typeWriter(typingContent[1], mainSubheading);
-        });
+    setInterval(flicker, 100);
+    setInterval(bigFlicker, 150);
+    const mainHeading = document.getElementById("main-heading");
+    const mainSubheading = document.getElementById("main-subheading");
+    const typingContent = [mainHeading.textContent, mainSubheading.textContent];
+    mainHeading.textContent = "";
+    mainSubheading.textContent = "";
+    typingSpeed = 50;
+    newLineSpeed = 250;
+    typeWriter(typingContent[0], mainHeading, 0, () => {
+        typeWriter(typingContent[1], mainSubheading);
+    });
 });
